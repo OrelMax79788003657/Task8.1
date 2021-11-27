8bd7f27c5aab9e52981d23e54652aa6f419ec15e
USER <USER@DESKTOP-FRBRUEK>
2021-11-27 21:52:28 +0300

    Добавьте файлы проекта.

---
+++ Task8.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\USER\Desktop\ИТМО\Lab8\Task8.1\Task8.1\bin\Debug";
+
+            string[] allFolders = Directory.GetDirectories(path);
+            string[] allFiles = Directory.GetFiles(path);
+            foreach (string files in allFiles)
+            {
+                Console.WriteLine(files);
+            }
+            foreach (string folder in allFolders)
+            {
+                Console.WriteLine();
+                Console.WriteLine(folder + ":");
+                allFiles = Directory.GetFiles(folder, "*.*");
+                foreach (string file in allFiles)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
